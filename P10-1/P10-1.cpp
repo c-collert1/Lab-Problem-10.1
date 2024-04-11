7cd97e2ac4d420a4bf4ef02c878fd4710de4558e
Caleb Collert <c-collert.1@onu.edu>
2024-04-11 09:47:28 -0400

    finished wordParser

--- P10-1/P10-1.cpp
+++ P10-1/P10-1.cpp
@@ -4,33 +4,47 @@
 using namespace std;
 
 string scramble(string word);
+void wordParser(string word);
 
 int main() {
+    srand(time(NULL));
 
-	srand(time(NULL));
+    string test = "I don't give a holstein for programmers that can only spell a variable one way. (Dr. Stephany)";
+    wordParser(test);
 
-	//string test = "I don't give a holstein for programmers that can only spell a variable one way. (Dr. Stephany)";
-	string test = "problem";
-	cout << scramble(test);
-
-
+    return 0;
 }
 
 string scramble(string word) {
-	if (word.length() > 3) {
-		string letterHold = "test";
-		int firstLetter = rand() % word.length();
-		int secondLetter = rand() % word.length();
-		while (firstLetter == 0 || firstLetter == (word.length() - 1)) {
-			firstLetter = rand() % word.length();
-		}
-		while (secondLetter == firstLetter || secondLetter == 0 || secondLetter == (word.length() - 1)) {
-			secondLetter = rand() % word.length();
-		}
+    if (word.length() > 3) { 
+        int firstLetter = rand() % (word.length() - 3) + 1; 
+        int secondLetter = rand() % (word.length() - 2) + 1;
+        while (secondLetter == firstLetter) {
+            secondLetter = rand() % (word.length() - 2) + 1;
+        }
+        char letterHold = word[firstLetter];
+        word[firstLetter] = word[secondLetter];
+        word[secondLetter] = letterHold;
+    }
+    return word;
+}
 
-		letterHold[1] = word[firstLetter];
-		word[firstLetter] = word[secondLetter];
-		word[secondLetter] = letterHold[1];
-		return word;
-	}
+void wordParser(string word) {
+    const char space = ' ';
+    string partOfPhrase = "";
+    for (int i = 0; i < word.length(); ++i) {
+        if (word[i] != space) {
+            partOfPhrase += word[i];
+        }
+        else {
+            if (!partOfPhrase.empty()) {
+                cout << scramble(partOfPhrase);
+                partOfPhrase = "";
+            }
+            cout << space;
+        }
+    }
+    if (!partOfPhrase.empty()) {
+        cout << scramble(partOfPhrase);
+    }
 }
